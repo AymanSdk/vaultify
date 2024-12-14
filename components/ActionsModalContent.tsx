f2c932dfa5a5cddaed1d2e0ae06e661444a7ac92
Aymansdk <ayman.sdk123@outlook.com>
2024-12-14 21:14:35 +0100

    Add ShareInput component for file sharing functionality in ActionsModal

--- components/ActionsModalContent.tsx
+++ components/ActionsModalContent.tsx
@@ -3,6 +3,9 @@ import FormattedDateTime from "./FormattedDateTime";
 import { convertFileSize, formatDateTime } from "@/lib/utils";
 
 import Thumbnail from "./Thumbnail";
+import Image from "next/image";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 
 // Helper component to display image thumbnail with date and time of creation
 const ImageThumbnail = ({ file }: { file: Models.Document }) => (
@@ -33,6 +36,64 @@ export const FileDetails = ({ file }: { file: Models.Document }) => {
         <DetailRow label="Size:" value={convertFileSize(file.size)} />
         <DetailRow label="Owner:" value={file.owner.fullName} />
         <DetailRow label="Last edit:" value={formatDateTime(file.$updatedAt)} />
+      </div>
+    </>
+  );
+};
+
+// Share Modal Component to handle Share Functionality in the DropDown Actions
+
+interface Props {
+  file: Models.Document;
+  onInputChange: React.Dispatch<React.SetStateAction<string[]>>;
+  onRemove: (email: string) => void;
+}
+
+export const ShareInput = ({ file, onInputChange, onRemove }: Props) => {
+  return (
+    <>
+      <ImageThumbnail file={file} />
+      <div className="share-wrapper">
+        <p className="subtitle-2 pl-1 text-light-100">
+          Share file with other users
+        </p>
+        <Input
+          type="email"
+          placeholder="Enter email address"
+          onChange={(e) => onInputChange(e.target.value.trim().split(","))}
+          className="share-input-field"
+        />
+        <div className="pt-4">
+          <div className="flex justify-between">
+            <p className="subtitle-2 text-light-100">Shared with</p>
+            <p className="subtitle-2 text-light-200">
+              {file.users.length} users
+            </p>
+          </div>
+
+          <ul className="pt-2">
+            {file.users.map((email: string) => (
+              <li
+                key={email}
+                className="flex items-center justify-between gap-2"
+              >
+                <p className="subtitle-2">{email}</p>
+                <Button
+                  onClick={() => onRemove(email)}
+                  className="share-remove-user"
+                >
+                  <Image
+                    src="/assets/icons/remove.svg"
+                    alt="remove"
+                    width={24}
+                    height={24}
+                    className="remove-icon"
+                  />
+                </Button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   );
